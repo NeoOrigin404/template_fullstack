50dded38322db8e0df661679d183c095109a2ab0
Julien PICART <julienpicart01@gmail.com>
2025-03-04 14:05:13 +0100

    In progress backend

---
+++ server/src/router.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const router = express.Router();
+
+/* ************************************************************************* */
+// Define Your API Routes Here
+/* ************************************************************************* */
+
+export default router;
